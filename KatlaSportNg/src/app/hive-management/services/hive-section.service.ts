e563bd18bbc238384c39575e2c9cd8ca670ab419
Anastasiya Galtsova <galtsova98@gmail.com>
2018-06-19 22:17:08 +0300

    Merge step14. Add addHiveSection, updateHiveSection and deleteHiveSection methods to HiveSectionService. Add fields to HiveSection. Add a layout to HiveSectionFormComponent. Add onSubmit, onDelete, onUndelete and onPurge methods to HiveSectionFormComponent.

--- KatlaSportNg/src/app/hive-management/services/hive-section.service.ts
+++ KatlaSportNg/src/app/hive-management/services/hive-section.service.ts
@@ -24,4 +24,16 @@ export class HiveSectionService {
   setHiveSectionStatus(hiveSectionId: number, deletedStatus: boolean): Observable<Object> {
     return this.http.put<Object>(`${this.url}${hiveSectionId}/status/${deletedStatus}`, null);
   }
+
+  addHiveSection(hiveId: number, hiveSection: HiveSection): Observable<HiveSection> {
+    return this.http.post<HiveSection>(`${this.url}${hiveId}`, hiveSection);
+  }
+
+  updateHiveSection(hiveSection: HiveSection): Observable<Object> {
+    return this.http.put<Object>(`${this.url}${hiveSection.id}`, hiveSection);
+  }
+
+  deleteHiveSection(hiveId: number): Observable<Object> {
+    return this.http.delete<Object>(`${this.url}${hiveId}`);
+  }
 }
